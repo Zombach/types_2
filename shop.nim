2c779a4903f171821b078da2f6bdea516159a7a1
ZLoo <Kozlov.s.v.1992@gmail.com>
2025-01-26 09:47:21 +0300

    shop

--- shop.nim
+++ shop.nim
@@ -3,7 +3,7 @@ import shop/[functions]
 when isMainModule:
   initStaff(staff, "Ivan", "Ivanov", "22.02.1998", "Кассир")
   echo staff
-  initGood(good, "Title", 100, "01.01.1970", 0.33, 5)
+  initGood(good, "Title", 100.0, "01.01.1970", 0.33, 5)
   echo good
   initCash(cash, 55, false, 55_333.44)
   echo cash
